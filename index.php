aa943bd7a3ff0d6fef3eb43c8f321c63fff5468e
altheaabadilla2599 <70379536+altheaabadilla2599@users.noreply.github.com>
2020-09-25 12:13:13 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<link rel="stylesheet" type="text/css" href="css/content.css">
+
+
+
+
+
+<div class="box">
+
+<div class="text2"><center>Creamy Creations </center></div>
+</div>
+ <div class="holder">
+    <div class="shadow"></div>
+    <div class="cake">
+      <div class="first"></div>
+      <div class="frost"></div>
+      <div class="frost2"></div>
+      <div class="frost3"></div>
+    </div>
+  </div>
+
+
+
+<div class="txt"> Joy Full of Sweet Moments
+					<p>Delight in every BITE.</p>
+</div>
+<br>
+<div class="txt1">
+" Love is like a good cake, you never know when it's
+<p> coming, but you'd better eat it when it does!"</p>
+</div>
+<br>
+
+<div class="txt2">
+	- C. JoyBell C. 
+</div>
+
+
+
+<button class="button">Gallery </button>
+<button class="button2">Reviews</button>
+
+
+
+	 <div class="cupcake">
+  <div class="frosting">
+ 
+    <span class="top"></span>
+    <span class="middle"></span>
+    <span class="bottom"></span>
+   </div>
+   <div class="cake1"></div>
+  <div class="cup">
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+ </div>
+ 
+ 
+<div class="sayings">About us</div>
+<div class="sayings2">It all started with a creamy taste of cake that my mother bakes for thanksgiving.
+Creamy Creations was created because of an inspiration.
+<p>We are glady to serve you one of the best cakes that you could imagine.</p>
+<p>Any flavors that you wished and design will be granted with Creamy Creations. Cakes are special every birthday, every celebrations ends</p>
+ <p>with something sweet. It's all about memories</div>	
+<div class="heart"></div>
+
+
+ 
+
+ 
+	<div class="cake-box">
+	<div class="second"></div>
+    <div class="terd"></div>
+    <div class="pers">
+	
+    
+      
+    </div>
+	</div>
+	
+   
+   
+	
+   
+   
+ 
+ 
+ 
+ 	 
+ 
+ 
+
+
+
+
+	
+	
+
+					 
+ 
